5b08ffd817393fc65e36b6e71b02690aa2eb305d
nandovallec <nandovallec@gmail.com>
2020-03-01 00:35:00 +0100

    In progress

---
+++ P1/GVGAI/src/tsi/Agent.java
@@ -0,0 +1,23 @@
+package tsi;
+
+import com.sun.org.apache.bcel.internal.generic.Type;
+
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+
+public class Agent extends AbstractPlayer {
+    public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+    	
+    }
+    
+    public void init(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+    	
+    }
+
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+    	return Types.ACTIONS.ACTION_ESCAPE;
+    }
+
+}
